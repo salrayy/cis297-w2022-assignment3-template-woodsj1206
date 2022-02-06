4b4b0f88e3627268fe94fb6c83d98d5392f1caa0
woodsj1206 <woods.j1206@gmail.com>
2022-02-05 21:31:43 -0500

    Committed changes

---
+++ BankApplication/CheckingAccount.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    /// <summary>
+    /// Represents a users checkings account.
+    /// </summary>
+    class CheckingAccount : Account
+    {
+        private decimal chargedFee;
+
+        /// <summary>
+        /// Property for getting and setting the Charged Fee.
+        /// </summary>
+        public decimal ChargedFee
+        {
+            get => chargedFee;
+
+            set
+            {
+                if (value >= 0.0M)
+                {
+                    chargedFee = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public CheckingAccount()
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor that sets the account balance and charged fee.
+        /// </summary>
+        /// <param name="balance"></param>
+        /// <param name="fee"></param>
+        public CheckingAccount(decimal balance, decimal fee)
+        {
+            Balance = balance;
+            ChargedFee = fee;
+        }
+
+        /// <summary>
+        /// Deposits money into the account.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns> Returns true or false based on the success of the operation. </returns>
+        public override bool Credit(decimal amount)
+        {
+            if (base.Credit(amount))
+            {
+                Balance -= ChargedFee;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Withdraws money from the account.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns> Returns true or false based on the success of the operation. </returns>
+        public override bool Debit(decimal amount)
+        {
+            if (base.Debit(amount))
+            {
+                Balance -= ChargedFee;
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -6,10 +6,87 @@ using System.Threading.Tasks;
 
 namespace BankApplication
 {
+    /// <summary>
+    /// This program uses an Account hierachy to implement a banking application.
+    /// </summary>
+    /// <Student>Jonathan Woods</Student>
+    /// <Class>CIS297</Class>
+    /// <Semester>Winter 2022</Semester>
     class Program
     {
+        /// <summary>
+        /// The program allows a user to deposit and withdraw from savings and checking accounts.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            var accounts = new List<Account>() {new SavingsAccount(1000M, 0.05M), new CheckingAccount(300M, 5M), new SavingsAccount(1250M, 0.02M), new CheckingAccount(1500M, 3M) };
+
+            int accountNumber = 0;
+            string accountType;
+
+            foreach (var currentAccount in accounts)
+            {                
+                accountNumber++;
+
+                if (currentAccount is SavingsAccount)
+                {
+                    var account = (SavingsAccount)currentAccount;
+
+                    accountType = "Savings";
+
+                    UpdateAccount(account, accountNumber, accountType);
+
+                    account.Credit(account.CalculateInterest());
+                }
+                else
+                {
+                    var account = (CheckingAccount)currentAccount;
+
+                    accountType = "Checking";
+
+                    UpdateAccount(account, accountNumber, accountType);
+                }
+
+                Console.WriteLine($"The updated balance of {accountType} Account #{accountNumber} is: ${currentAccount.Balance}\n");
+            }
+
+            Console.WriteLine("Press \"Enter\" to end the program.");
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Updates the balance of an account after the user withdraws or deposits money.
+        /// </summary>
+        /// <param name="account"></param>
+        /// <param name="accountNumber"></param>
+        /// <param name="accountType"></param>
+        private static void UpdateAccount(Account account, int accountNumber, string accountType = "")
+        {
+            Console.WriteLine($"The current balance of {accountType} Account #{accountNumber} is: ${account.Balance}");
+
+            Console.Write($"Enter the amount of money you would like to withdraw from {accountType} Account #{accountNumber}: $");
+
+            if (decimal.TryParse(Console.ReadLine(), out decimal enteredAmount))
+            {
+                account.Debit(enteredAmount);
+            }
+            else
+            {
+                Console.WriteLine("Invalid amount.");
+            }
+
+            Console.Write($"Enter the amount of money you would like to deposit into {accountType} Account #{accountNumber}: $");
+
+            if (decimal.TryParse(Console.ReadLine(), out enteredAmount))
+            {
+                account.Credit(enteredAmount);
+            }
+            else
+            {
+                Console.WriteLine("Invalid amount.");
+            }
         }
     }
 }
